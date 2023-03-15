87b47ce54ac3629e5dbb915c26e0f3e6a6fb6601
unknown <kuprianovpavel9@gmail.com>
2023-03-15 22:43:28 +0300

    15.03

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const arrColor = ['red', 'yellow', 'green', 'orange', 'blue', 'pink', 'purple' ]
+
 const btnClose = document.querySelector('.modal-btn-start');
 const modal = document.querySelector('.modal');
 const h2 = document.querySelector('.title-h2');
+
+const verify = document.querySelector('.verify');
 
 const modalViewHandler = () => {
     modal.classList.toggle('modal--open');
@@ -24,15 +28,18 @@ btnClose.addEventListener('click', (event) => {
         modalViewHandler();
     } );
 })
-
-
+let z = 0;
+let gameScore = 0;
 const level1 = document.querySelector('.level1');
 let result = [];
 let n = 0
+const levelBlock = document.querySelector('.level');
 
 function addLevelExample(level, sign) {
-    const levelBlock = document.querySelector('.level');
-    const textExample = levelBlock.querySelectorAll('text');
+    
+    const textExample = level.querySelectorAll('text');
+    let quantityExamples = textExample.length
+    console.log(quantityExamples)
     textExample.forEach((itemText)=> {
         let a = Math.floor(Math.random()*10);
         let b = Math.floor(Math.random()*10);
@@ -79,73 +86,108 @@ function addLevelExample(level, sign) {
         console.log(result[n]);
         let dataSet = itemText.dataset;
         dataSet.result = result[n];
-        itemText.addEventListener('click', (e) => {
-            console.log("Выбрали текст - ", e.target)
-            // let dataSet = itemText.dataset.ball;
-
-            document.getElementById('textStart').style.display = 'none';
-            document.getElementById('textChoice').style.display = 'block';
-            document.querySelector('.answer').style.display = 'block';
-            document.querySelector('.cover').style.display = 'block';
-
-            let ballTextExample;
-
-            levelBlock.querySelectorAll(`[data-ball = ${dataSet.ball}]`).forEach((item) => {
-                item.style.stroke = 'orange';
-                ballTextExample = item;
-            })
-
-
-
-            console.log("ballTextExample ", ballTextExample)
-            console.log("1 вариант - ", dataSet.result)
-            const verify = document.querySelector('.verify');
-            const decision = document.getElementById('decision')
-            let hren = ballTextExample.dataset.result;
-
-
-            verify.addEventListener('click', (e) => {
-
-                console.log(ballTextExample.dataset.result);
-                console.log(hren);
-                e.preventDefault();
-                // e.stopPropagation();
-                let resultForm = decision.value;
-                console.log(resultForm)
-                if (resultForm === dataSet.result) {
-                    levelBlock.querySelectorAll(`[data-ball = ${dataSet.ball}]`).forEach((item) => {
-                        item.style.stroke = 'red';
-                        item.style.fill = 'red';
-                    });
-                    document.querySelector('.answer').style.display = 'none';
-                    document.querySelector('.cover').style.display = 'none';
-                    document.getElementById('textStart').style.display = 'block';
-                    document.getElementById('textChoice').style.display = 'none';
-                    ballTextExample.style.display = 'none';
-                    decision.value = '';
-                    console.log('Верно!');
-                    console.log(result)
-                } else {
-                    console.log('неправильно!!!!');
-                    document.querySelector('.title-error').style.display = 'block';
-                    document.querySelector('.answer').style.display = 'none';
-                    setTimeout(() => {
-                        document.querySelector('.title-error').style.display = 'none';
-                        document.querySelector('.answer').style.display = 'block';
-                    }, 2000);
-                    // document.getElementById("answerform").reset();
-                }
-
-                // document.forms.answerForm.elements.answer.value = '';
-            })
-
-
-        })
+        console.log('itemText - ', itemText);
 
         n++;
     })
-    console.log(result)
+
+
+    function controlClick() {
+        document.getElementById('textStart').style.display = 'none';
+        document.getElementById('textChoice').style.display = 'block';
+        document.querySelector('.answer').style.display = 'block';
+        document.querySelector('.cover').style.display = 'block';
+
+    }
+
+    let textTags = document.querySelectorAll('text');
+
+    textTags.forEach((textTag)=> {
+        console.log(result.length)
+        textTag.addEventListener('click', (e) => {
+            resultTextExample = e.target.dataset.result;
+            let eventTextBlock = e.target
+            controlClick();
+
+            levelBlock.querySelectorAll(`[data-ball = '${e.target.dataset.ball}']`).forEach((item) => {
+                item.style.stroke = 'orange';
+            })
+
+            let ballExample = e.target.dataset.ball;
+
+            calculate(resultTextExample, ballExample, eventTextBlock, z)
+            z++;
+            console.log('z - ',z)
+        });
+
+    })
 
 }
 addLevelExample(level1, 'plus');
+
+
+function calculate(resultTextExample, ballExample, eventTextBlock, z) {
+    const verify = document.querySelector('.verify');
+    const input = document.getElementById('inputForm');
+    input.value = '';
+
+    verify.addEventListener('click', buttonHandler);
+
+
+    function buttonHandler(e) {
+        e.stopPropagation()
+
+        const answer = input.value;
+        console.log(ballExample);
+
+        if (resultTextExample == answer) {
+            levelBlock.querySelectorAll(`[data-ball = '${ballExample}']`)
+                .forEach((item) => {
+                    console.log(ballExample)
+                item.style.stroke = arrColor[ballExample];
+                item.style.fill = arrColor[ballExample];
+            })
+            gameScore +=10;
+                document.querySelector('.answer').style.display = 'none';
+                document.querySelector('.cover').style.display = 'none';
+                document.getElementById('textStart').style.display = 'block';
+                document.getElementById('textChoice').style.display = 'none';
+                eventTextBlock.style.display = 'none';
+                document.querySelector('.title-correctly').style.display = 'block';
+                document.querySelector('.answer').style.display = 'none';
+                setTimeout(() => {
+                    document.querySelector('.title-correctly').style.display = 'none';
+                }, 1000);
+                resultTextExample = '';
+                input.value ='';
+                console.log('Верно!');
+                console.log(z);
+                if (Number(z+1)=== result.length) {
+                    document.getElementById('textStart').style.display = 'none';
+                    document.getElementById('textOffer').style.display = 'none';
+                    document.querySelector('.title-result').style.display = 'block';
+                    document.querySelector('.title-score').style.display = 'block';
+                    document.querySelector('.title-score').textContent = `Общий счёт - ${gameScore}`;
+                }
+
+        } else {
+                console.log('неправильно!!!!');
+                document.querySelector('.title-error').style.display = 'block';
+                document.querySelector('.answer').style.display = 'none';
+                input.value ='';
+                setTimeout(() => {
+                    document.querySelector('.title-error').style.display = 'none';
+                    document.querySelector('.answer').style.display = 'block';
+                }, 2000);
+
+                gameScore -=1;
+                buttonHandler()
+        }
+        document.getElementById('verify').removeEventListener('click', buttonHandler)
+        return z
+    }
+
+
+
+}
 
